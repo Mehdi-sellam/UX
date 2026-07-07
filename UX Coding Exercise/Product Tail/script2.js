303854a9723afb44414b8af7c8844ee5af2e1413
mehdi-sellam <mehdisellam03@gmail.com>
2026-07-07 22:39:25 +0100

    fix(multiple tiles page): correct thumbnail swap
    - add a temporary variable to safely swap main and thumbnail image sources when a thumbnail is clicked

--- UX Coding Exercise/Product Tail/script2.js
+++ UX Coding Exercise/Product Tail/script2.js
@@ -6,6 +6,8 @@ const thumbnails = document.querySelectorAll('.thumbnail');
 // event listener to each thumbnail
 thumbnails.forEach(thumbnail => {
     thumbnail.addEventListener('click', () => {
+        const tempSrc = mainImage.src;
         mainImage.src = thumbnail.src;
+        thumbnail.src = tempSrc;
     });
 });
